3700380f4f77ef6aa1f1aa7a66a765a45333270b
Travis-Taylor <travis.taylor@patternlabs.ai>
2023-01-11 12:22:55 -0700

    Fix setup

--- .vimrc
+++ .vimrc
@@ -7,5 +7,5 @@ set shiftwidth=4
 
 filetype plugin indent on
 execute pathogen#infect()
-# Git blame plugin: https://github.com/zivyangll/git-blame.vim
+" Git blame plugin: https://github.com/zivyangll/git-blame.vim
 nnoremap <Leader>b :<C-u>call gitblame#echo()<CR>
--- setup.sh
+++ setup.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
+function link_config {
+    # Link in config file to ~ dir if it doesn't already exist
+    [[ -f ~/"$1" ]] || ln -s $(pwd)/"$1" ~
+}
+
 # Install vim pathogen plugin handler: https://github.com/tpope/vim-pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+[[ -f ~/.vim/autoload/pathogen.vim ]] || \
+    (mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim)
 
 # Install git blame plugin: https://github.com/zivyangll/git-blame.vim
-cd ~/.vim/bundle && \
-    git clone git@github.com:zivyangll/git-blame.vim.git
+[[ -d ~/.vim/bundle/git-blame.vim ]] || (cd ~/.vim/bundle && \
+    git clone git@github.com:zivyangll/git-blame.vim.git)
 
-#TODO: fix syntax
-#find . -maxdepth 1 -type f ! -name README.md | xargs -I{} ln -s $(pwd)/{} ~
+# Link top-level config files into $HOME
+configs=($(find . -maxdepth 1 -type f ! -name README.md ! -name setup.sh))
+for f in "${configs[@]}"; do
+    link_config "$f"
+done
+[[ -f /etc/modprobe.d/disable-nouveau.conf ]] || \
+    sudo ln -s $(pwd)/etc/modprobe.d/disable-nouveau.conf /etc/modprobe.d
