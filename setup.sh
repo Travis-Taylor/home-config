54686def4337f88b5f0cef02ac8c829f26bea1b5
Travis-Taylor <stygian.stevedore@gmail.com>
2024-06-14 20:16:19 -0600

    Install Vundle

--- setup.sh
+++ setup.sh
@@ -12,19 +12,37 @@ function link_config {
         ln -s "$1" ~
     fi
 }
+# Install some software
+snapps=(code slack spotify vlc)
+for snapp in "${snapps[@]}"; do
+    if [[ -z $(snap list | grep "^$snapp\s") ]]; then
+        sudo snap install "$snapp"
+    fi
+done
+
+debs=(curl git terminator ack vim git git-lfs inkscape)
+for deb in "${debs[@]}"; do
+    if ! dpkg-query --show --showformat='${db:Status-Status}\n' "$deb" | grep -q "^installed"; then
+        sudo apt install -y "$deb"
+    fi
+done
 
 # Install vim pathogen plugin handler: https://github.com/tpope/vim-pathogen
 if [[ ! -f ~/.vim/autoload/pathogen.vim ]]; then
     mkdir -p ~/.vim/autoload ~/.vim/bundle
-    which curl > /dev/null || sudo apt install -y curl
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
 # Install git blame plugin: https://github.com/zivyangll/git-blame.vim
 if [[ ! -d ~/.vim/bundle/git-blame.vim ]]; then
     cd ~/.vim/bundle
-    which git > /dev/null || sudo apt install -y git
     git clone git@github.com:zivyangll/git-blame.vim.git
+fi
+
+# Install vundle plugin handler: https://github.com/VundleVim/Vundle.vim
+if [[ ! -f ~/.vim/bundle/Vundle.vim ]]; then
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 # Link top-level config files into $HOME
@@ -47,21 +65,6 @@ if [[ ! -f "$HOME/.config/terminator/config" ]]; then
     sudo ln -s "${SCRIPT_DIR}"/terminator/config $HOME/.config/terminator
 
 fi
-
-# Install some software
-snapps=(code slack spotify vlc)
-for snapp in "${snapps[@]}"; do
-    if [[ -z $(snap list | grep "^$snapp\s") ]]; then
-        sudo snap install "$snapp"
-    fi
-done
-
-debs=(terminator ack vim git git-lfs inkscape)
-for deb in "${debs[@]}"; do
-    if ! dpkg-query --show --showformat='${db:Status-Status}\n' "$deb" | grep -q "^installed"; then
-        sudo apt install -y "$deb"
-    fi
-done
 
 # Install AWS CLI
 if ! command -v aws > /dev/null; then
